82f614c3bd3f0b418b930a99ca48e9c79c22df1d
Fathurozak Buhari <rozakbuhari@gmail.com>
2020-05-01 22:10:48 +0700

    Setup next app

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Start building your awesome product with</h1>
+      <div>
+        <a href="https://github.com/rozakbuhari/next-boilerplate">
+          <img alt="next-boilerplate" src="/images/logo.png" height="250px" />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
